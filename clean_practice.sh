46595172e126d8cb5024365ea2e922a13625fa19
Spencer <spencer3035@gmail.com>
2025-02-02 22:12:16 -0700

    Removed column from clean script

--- clean_practice.sh
+++ clean_practice.sh
@@ -8,8 +8,7 @@ cd "$dir/$practice_dir"
 for file in *.txt; do 
     # 'sed' removes leading and trailing whitespace, and deletes empty lines
     # 'sort | uniq' both sort and clean up possible duplicate entries
-    # 'column -t' aligns the entries nicely
-    sorted_string=$(cat $file | sed 's/^ *//; s/ *$//; /^$/d' | sort | uniq | column -t)
+    sorted_string=$(cat $file | sed 's/^ *//; s/ *$//; s/\s*/\t/; /^$/d' | sort | uniq)
     # This needs to be done as a separate step because of how pipes work
     echo "$sorted_string" > $file
 done
